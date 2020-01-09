9ba1ec299dc56fb2726fe2706cde91004c83f3cb
dell <dell@DESKTOP-BBKOFHC>
2020-01-09 15:33:01 +0530

    2nd Commit

--- src/BluestoneGoldminefilldetail.java
+++ src/BluestoneGoldminefilldetail.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BluestoneGoldminefilldetails {
+public class BluestoneGoldminefilldetail {
 	public static void main(String[] args) throws InterruptedException 
 	{
 		System.setProperty("webdriver.chrome.driver", ".\\Software\\chromedriver.exe");
